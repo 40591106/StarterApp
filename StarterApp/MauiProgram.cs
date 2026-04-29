4c730446464bebae923c4d02c4378477be30f58d
Dylan Dagnall <40591106@live.napier.ac.uk>
2026-04-29 21:55:30 +0000

    feat: reviews - submit, view, user profile, average rating, sync reviews migration

--- StarterApp/MauiProgram.cs
+++ StarterApp/MauiProgram.cs
@@ -20,7 +20,7 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
 
-        const bool useSharedApi = true;
+        const bool useSharedApi = false; // Set to true to use the shared API, false to use local database
 
         if (useSharedApi)
         {
